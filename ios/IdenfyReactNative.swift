7de27557476c05f27718cd6ee02e7ace94bfe1aa
Wouter Bohlken <bohlkenw@gmail.com>
2023-01-03 16:24:33 +0100

    iOS customization setup

--- ios/IdenfyReactNative.swift
+++ ios/IdenfyReactNative.swift
@@ -1,5 +1,7 @@
 import Foundation
 import iDenfySDK
+import idenfyviews
+
 @objc(IdenfyReactNative)
 class IdenfyReactNative: NSObject {
     
@@ -29,15 +31,43 @@ class IdenfyReactNative: NSObject {
                 .withAuthToken(authToken)
                 .build()
             
-            let idenfyController = IdenfyController.shared
-            idenfyController.initializeIdenfySDKV2WithManual(idenfySettingsV2: idenfySettingsV2)
+            let idenfyColorMain = "#EA9619"
+            let idenfyColorButton = "#6539AC"
+            IdenfyCommonColors.idenfyMainColorV2 = UIColor(hexString: idenfyColorMain)
+            IdenfyCommonColors.idenfyMainDarkerColorV2 = UIColor(hexString: idenfyColorMain)
+            IdenfyCommonColors.idenfyGradientColor1V2 = UIColor(hexString: idenfyColorButton)
+            IdenfyCommonColors.idenfyGradientColor2V2 = UIColor(hexString: idenfyColorButton)
+
+            IdenfyToolbarUISettingsV2.idenfyDefaultToolbarBackgroundColor = UIColor(hexString: idenfyColorMain)
+
+            IdenfyToolbarUISettingsV2.idenfyDefaultToolbarBackIconTintColor = IdenfyCommonColors.idenfyBlack
+            IdenfyToolbarUISettingsV2.idenfyDefaultToolbarLogoIconTintColor = IdenfyCommonColors.idenfyBlack
+
+            IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarLanguageSelectionIconTintColor  = IdenfyCommonColors.idenfyBlack
+            IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarCloseIconTintColor = IdenfyCommonColors.idenfyBlack
+
+            IdenfyCommonColors.idenfyPhotoResultDetailsCardBackgroundColorV2 = UIColor(hexString: "#FFE5BD")
+
+            IdenfyPhotoResultViewUISettingsV2.idenfyPhotoResultViewDetailsCardTitleColor = UIColor(hexString: idenfyColorButton)
+
             
+             let idenfyController = IdenfyController.shared
+            let faceReauthenticationInitialization = FaceAuthenticationInitialization(authenticationToken: authToken, withImmediateRedirect: false)
+            idenfyController.initializeFaceAuthentication(faceAuthenticationInitialization: faceReauthenticationInitialization)
+            
+            // let idenfyViewsV2:IdenfyViewsV2 = IdenfyViewsBuilderV2()
+            //     .withCountryCellView(CountryCell.self)
+            //     .build()
+
+            // let idenfyController = IdenfyController.shared
+            // idenfyController.initializeIdenfySDKV2WithManual(idenfySettingsV2: idenfySettingsV2, idenfyViewsV2: idenfyViewsV2)
+
             let idenfyVC = idenfyController.instantiateNavigationController()
-            
+
             idenfyVC.modalPresentationStyle = .fullScreen
-            
+
             UIApplication.shared.windows.first?.rootViewController?.present(idenfyVC, animated: true)
-            
+
             handleSdkCallbacks(idenfyController: idenfyController, resolver: resolve)
             
         } catch let error as NSError {
